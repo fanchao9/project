b7c050eb5b701da54a858653f210fd0627c803e1
fanchao <fanchaolin2005@gmail.com>
2025-05-22 16:04:39 -0400

    Cleared balldontlie api/layout change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { teams, nbaTeams, warriors, players, steph, steph2 } from "../../nba-api
 
 export default function Home() {
 
-  const teamNames = nbaTeams.map((team) => team.full_name);
-  console.log(warriors);
+  //const teamNames = nbaTeams.map((team) => team.full_name);
+  //console.log(warriors);
 
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex gap-4 items-center sm:items-start">
-        <div className="flex flex-col gap-4 bg-white text-black">
+        {/* <div className="flex flex-col gap-4 bg-white text-black">
           {teamNames.map((team) => (
             <div
               key={team}
@@ -37,7 +37,7 @@ export default function Home() {
             {steph.first_name} {steph.last_name} - {steph.team.full_name}
           </div>
         ))}
-        </div>
+        </div> */}
       </main>
     </div>
   );
